fb128b94dc6f9d6efb7df76110b8e5859851ac93
Artem Putilov <115434453+artemputilov@users.noreply.github.com>
2024-06-18 22:27:26 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ document.querySelector(".insult_btn").addEventListener("click", function() {
 //Задаём массив cо словами
 let randomCallings = ["супер", "ужасно", "мега", "ультра",]
 let randomAdjetives = ["мохнатый","вредный","ленивый","волосатый","огромный",];
-let randomAnimals = ["скотина","кабан","коняга","капибара","крюкохват", "жмыр", "макак"];
+let randomAnimals = ["скотина","кабан","коняга","капибара","крюкохват", "жмыр", "макак", "псина"];
 
 //Придаём рандом массиву через переменную
 let randomCalling = randomCallings [Math.floor(Math.random()*4)];
 let randomAdjetive = randomAdjetives [Math.floor(Math.random()*4)];
-let randomAnimal = randomAnimals [Math.floor(Math.random()*6)];
+let randomAnimal = randomAnimals [Math.floor(Math.random()*7)];
 
 //Выводим данные
 let randomInsult = ["Ты", randomCalling, randomAdjetive, randomAnimal].join (" ");
