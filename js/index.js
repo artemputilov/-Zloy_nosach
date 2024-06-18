b48f3f9912b38000127534a637ff83ddddf190e2
Artem Putilov <115434453+artemputilov@users.noreply.github.com>
2024-06-18 22:23:30 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.querySelector(".insult_btn").addEventListener("click", function() {
+
+//Задаём массив cо словами
+let randomCallings = ["супер", "ужасно", "мега", "ультра",]
+let randomAdjetives = ["мохнатый","вредный","ленивый","волосатый","огромный",];
+let randomAnimals = ["скотина","кабан","коняга","капибара","крюкохват", "жмыр", "макак"];
+
+//Придаём рандом массиву через переменную
+let randomCalling = randomCallings [Math.floor(Math.random()*4)];
+let randomAdjetive = randomAdjetives [Math.floor(Math.random()*4)];
+let randomAnimal = randomAnimals [Math.floor(Math.random()*6)];
+
+//Выводим данные
+let randomInsult = ["Ты", randomCalling, randomAdjetive, randomAnimal].join (" ");
+document.querySelector(".randomInsult").innerHTML = randomInsult;
+
+});
